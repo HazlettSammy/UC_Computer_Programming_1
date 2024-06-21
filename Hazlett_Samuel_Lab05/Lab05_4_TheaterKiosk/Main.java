f9325eb4253dd4b005197e36bdfb72c7bbaaf5a2
Samuel Hazlett <119336439+HazlettSammy@users.noreply.github.com>
2024-06-21 13:49:47 -0700

    Create Main.java

---
+++ Hazlett_Samuel_Lab05/Lab05_4_TheaterKiosk/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your age: ");
+        int age = scanner.nextInt();
+
+        if (age >= 21) {
+            System.out.println("You get a wristband.");
+        }
+        // No action needed for ages less than 21
+
+        scanner.close();
+    }
+}

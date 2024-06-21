f570eb77aa05343f897af81f9cdf572b23acf995
Samuel Hazlett <119336439+HazlettSammy@users.noreply.github.com>
2024-06-21 10:41:19 -0700

    Create subtractTwoNumbers.java

---
+++ Hazlett_Samuel_Lab08/subtractTwoNumbers.java
@@ -0,0 +1,7 @@
+public class subtractTwoNumbers
+{
+    // Method to subtract two numbers
+    public static int subtractTwoNumbers(int num1, int num2, int num3) {
+        return num1 - num2 - num3;
+    }
+}
